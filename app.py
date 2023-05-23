689df58dd4640b39321ccb03d963315cccdf1546
hj <2413302357@qq.com>
2023-05-23 13:26:11 +0800

    add static and templates

--- app.py
+++ app.py
@@ -1,7 +1,11 @@
-from flask import Flask
+from flask import Flask,render_template
 
 app = Flask(__name__)
 
+NAME = 'hj'
+MOVIES = [{'title':'银河护卫队','year':2023,'view_date':"1999-1-23"},
+          {'title':'复仇者联盟','year':2011,'view_date':"1999-12-31"}]
+
 @app.route('/')
-def hello():
-    return 'Welcome to my video list!!!!'
+def index():
+    return render_template('index.html',name = NAME,movies = MOVIES)
